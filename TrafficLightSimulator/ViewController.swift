4b2c0ac3412d049a3e08625867533f47e48a56b6
AndreySandler <mrdreem13@gmail.com>
2022-09-16 21:38:09 +0300

    Make the button colorful

--- TrafficLightSimulator/ViewController.swift
+++ TrafficLightSimulator/ViewController.swift
@@ -31,12 +31,18 @@ class ViewController: UIViewController {
         if counter == 1 {
             trafficLightGreenView.alpha = 0.3
             trafficLightRedView.alpha = 1
+            startButton.setTitle("Red", for: .normal)
+            startButton.backgroundColor = #colorLiteral(red: 1, green: 0.2332399487, blue: 0.1861645281, alpha: 1)
         } else if counter == 2 {
             trafficLightRedView.alpha = 0.3
             trafficLightYellowVIew.alpha = 1
+            startButton.setTitle("Yellow", for: .normal)
+            startButton.backgroundColor = #colorLiteral(red: 1, green: 0.8001160026, blue: 0.006338531151, alpha: 1)
         } else if counter == 3 {
             trafficLightYellowVIew.alpha = 0.3
             trafficLightGreenView.alpha = 1
+            startButton.setTitle("Green", for: .normal)
+            startButton.backgroundColor = #colorLiteral(red: 0.2066814005, green: 0.7795598507, blue: 0.349144876, alpha: 1)
             counter = 0
         }
     }
