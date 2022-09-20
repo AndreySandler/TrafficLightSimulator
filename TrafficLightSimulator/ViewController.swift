d6decf6f175508b6709c418880afbad74d035f35
AndreySandler <mrdreem13@gmail.com>
2022-09-20 15:15:00 +0300

    Fixed size for all screens

--- TrafficLightSimulator/ViewController.swift
+++ TrafficLightSimulator/ViewController.swift
@@ -9,44 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var trafficLightRedView: UIView!
-    @IBOutlet var trafficLightYellowVIew: UIView!
-    @IBOutlet var trafficLightGreenView: UIView!
+    @IBOutlet var lightRedView: UIView!
+    @IBOutlet var lightYellowVIew: UIView!
+    @IBOutlet var lightGreenView: UIView!
+    
     @IBOutlet var startButton: UIButton!
+    
     @IBOutlet var scoreLabel: UILabel!
+    
+    private var lightIsOn: CGFloat = 1
+    private var lightIsOff: CGFloat = 0.3
     
     private var counter = 0
     private var scoreCounter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        trafficLightRedView.layer.cornerRadius = 90
-        trafficLightYellowVIew.layer.cornerRadius = 90
-        trafficLightGreenView.layer.cornerRadius = 90
+        
         startButton.layer.cornerRadius = 10
     }
     
+    override func viewWillLayoutSubviews() {
+        lightRedView.layer.cornerRadius = lightRedView.frame.size.width / 2
+        lightYellowVIew.layer.cornerRadius = lightYellowVIew.frame.size.width / 2
+        lightGreenView.layer.cornerRadius = lightGreenView.frame.size.width / 2
+    }
+    
     @IBAction func pressingStartButton() {
-        startButton.setTitle("Next", for: .normal)
         scoreLabel.text = String(scoreCounter)
+        
         counter += 1
         scoreCounter += 1
         
         scoreLabel.isHidden == true ? scoreLabel.isHidden.toggle() : nil
         
+        if startButton.currentTitle == "Start" {
+            startButton.setTitle("Next", for: .normal)
+        }
+        
         if counter == 1 {
-            trafficLightGreenView.alpha = 0.3
-            trafficLightRedView.alpha = 1
+            lightGreenView.alpha = lightIsOff
+            lightRedView.alpha = lightIsOn
             startButton.setTitle("Red", for: .normal)
             startButton.backgroundColor = #colorLiteral(red: 1, green: 0.2332399487, blue: 0.1861645281, alpha: 1)
         } else if counter == 2 {
-            trafficLightRedView.alpha = 0.3
-            trafficLightYellowVIew.alpha = 1
+            lightRedView.alpha = lightIsOff
+            lightYellowVIew.alpha = lightIsOn
             startButton.setTitle("Yellow", for: .normal)
             startButton.backgroundColor = #colorLiteral(red: 1, green: 0.8001160026, blue: 0.006338531151, alpha: 1)
-        } else if counter == 3 {
-            trafficLightYellowVIew.alpha = 0.3
-            trafficLightGreenView.alpha = 1
+        } else {
+            lightYellowVIew.alpha = lightIsOff
+            lightGreenView.alpha = lightIsOn
             startButton.setTitle("Green", for: .normal)
             startButton.backgroundColor = #colorLiteral(red: 0.2066814005, green: 0.7795598507, blue: 0.349144876, alpha: 1)
             counter = 0
